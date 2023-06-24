5b45f9544d530c4fe800463c48b45129df67a247
Val Currie <contact@valcurrie.com>
2023-06-24 15:50:52 -0500

    simple autocomplete with a trie

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import './style.css'
 import Alpine from 'alpinejs';
+import {
+    BehaviorSubject,
+    Subject,
+    Subscription,
+    combineLatestWith,
+    distinctUntilChanged,
+    map, withLatestFrom, scan, startWith, pairwise,
+} from "rxjs";
+import { Trie } from "./trie.ts";
+import './style.css'
 
 declare global {
     interface Window {
@@ -14,8 +23,81 @@ interface Message {
 }
 
 Alpine.data('balmi', () => {
+    const wordList = [
+        'foo',
+        'foobar',
+        'baz'
+    ];
+    
+    const subscriptions: Subscription[] = [];
+
+    const completions$ = new BehaviorSubject(new Trie(wordList));
+    const completionPrefix$ = new Subject<string>();
+    const localCompletions$ = completionPrefix$.pipe(
+        distinctUntilChanged(),
+        combineLatestWith(completions$),
+        map(([prefix, completions]): string[] => {
+            if (prefix === '') return [] as string[];
+            return completions.autocomplete(prefix);
+        }),
+    );
+    const incrementCompIdx$ = new Subject<void>();
+    const compIdx$ = incrementCompIdx$.pipe(
+        withLatestFrom(
+            completionPrefix$.pipe(
+                startWith(null),
+                pairwise(),
+                map(([prev,next]) => prev !== next)
+            ),
+            localCompletions$.pipe(map(arr => arr.length))
+        ),
+        scan((compIdx, [, prefixChanged, compLength]) => {
+            if (prefixChanged) return 0;
+            if (++compIdx >= compLength) return 0;
+            return compIdx;
+        }, 0),
+    );
+
     return {
         rows: [{ type: 'meta', text: `> Started ${new Date().toISOString()}` }] as Message[],
+        chatInputEl() {
+            return this.$refs.chatInputEl as HTMLInputElement;
+        },
+        chatInput: '',
+        onChat() {
+            if (!this.chatInput) return;
+            this.rows.unshift({type: 'chat', text: this.chatInput});
+            this.chatInput = '';
+        },
+
+        compIdx: 0,
+        localCompletions: [],
+        onComplete(idx?: number) {
+            if (typeof idx === 'number') {
+                this.chatInput = this.localCompletions[idx];
+                this.clearComplete();
+                this.chatInputEl().focus();
+                return;
+            }
+
+            completionPrefix$.next(this.chatInput);
+            incrementCompIdx$.next();
+        },
+        clearComplete() {
+            completionPrefix$.next('');
+        },
+
+        init() {
+            compIdx$.subscribe(compIdx => this.compIdx = compIdx);
+
+            completionPrefix$.next('');
+            localCompletions$.subscribe(localCompletions => this.localCompletions = localCompletions);
+        },
+        destroy() {
+            while (subscriptions.length) {
+                subscriptions.pop()!.unsubscribe();
+            }
+        },
     }
 });
 
---
+++ src/trie.ts
@@ -0,0 +1,64 @@
+// source: https://www.lavivienpost.com/autocomplete-with-trie-code/
+
+class TrieNode {
+    data: string;
+    isEnd: boolean;
+    children: Map<string, TrieNode>;
+
+    //Constructor, Time O(1), Space O(1)
+    constructor(c: string) {
+        this.data = c;
+        this.isEnd = false;
+        this.children = new Map(); //map
+    }
+}
+
+export class Trie {
+    private root: TrieNode;
+
+    //Constructor, Time O(1), Space O(1)
+    constructor(wordList?: string[]) {
+        this.root = new TrieNode('');
+        if (wordList?.length) wordList.forEach(w => this.insert(w));
+    }
+
+    //inserts a word into the trie. Time O(s), Space O(s), s is word length
+    public insert (word: string) {
+        let node = this.root;
+        for (let ch of word) {
+            if (!node.children.has(ch))
+                node.children.set(ch, new TrieNode(ch));
+            node = node.children.get(ch)!;
+        }
+        node.isEnd = true;
+    }
+
+    //find all word with given prefix, call recursion function
+    //Time O(n), Space O(n), n is number of nodes involved (include prefix and branches)
+    public autocomplete (word: string) {
+        let res: string[] = [];
+        let node = this.root;
+        for (let ch of word) {
+            if (node.children.has(ch))
+                node = node.children.get(ch)!;
+            else
+                return res;
+        }
+        this.helper(node, res, word.substring(0, word.length-1));
+        return res;
+    }
+
+    //recursive function called by autocomplete
+    //Time O(n), Space O(n), n is number of nodes in branches
+    private helper (node: TrieNode, res: string[], prefix: string)  {
+        if (node.isEnd)
+            res.push(prefix + node.data);
+        for (let c of node.children.keys())
+            if (node.children.has(c))
+                this.helper(
+                    node.children.get(c)!,
+                    res,
+                    prefix + node.data
+                );
+    }
+}
