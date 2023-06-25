b61acf943e0c057b869fd3cfe88e6866af19babf
Val Currie <contact@valcurrie.com>
2023-06-24 22:16:20 -0500

    put autocompleter into a 'hook'

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import {
     Subscription,
     combineLatestWith,
     distinctUntilChanged,
-    map, withLatestFrom, scan, startWith, pairwise,
+    map, withLatestFrom, scan, startWith, pairwise, combineLatest,
 } from "rxjs";
 import { Trie } from "./trie.ts";
 import './style.css'
+import {useAutocompleter} from "./autocompleter.ts";
 
 declare global {
     interface Window {
@@ -22,45 +23,22 @@ interface Message {
     text: string;
 }
 
+
 Alpine.data('balmi', () => {
     const wordList = [
         'balmi says hello',
+        'ding'
     ];
     
     const subscriptions: Subscription[] = [];
 
-    const completions$ = new BehaviorSubject(new Trie(wordList));
-    const completionPrefix$ = new Subject<string>();
-    const incrementCompIdx$ = new Subject<void>();
-
-    // performs autocomplete by combining latest distinct prefix with latest completion trie
-    const localCompletions$ = completionPrefix$.pipe(
-        distinctUntilChanged(),
-        combineLatestWith(completions$),
-        map(([prefix, completions]): string[] => {
-            if (prefix === '') return [] as string[];
-            return completions.autocomplete(prefix);
-        }),
-    );
-
-    // tracks the current completion index
-    const compIdx$ = incrementCompIdx$.pipe(
-        withLatestFrom(
-            completionPrefix$.pipe(
-                startWith(null),
-                pairwise(),
-                map(([prev,next]) => prev !== next)
-            ),
-            localCompletions$.pipe(map(arr => arr.length))
-        ),
-        scan((compIdx, [, prefixChanged, compLength]) => {
-            if (prefixChanged) return 0;
-            if (++compIdx >= compLength) return 0;
-            return compIdx;
-        }, 0),
-    );
-
     const initialMessage = { type: 'meta', text: `> Started ${new Date().toISOString()}` };
+
+    const {
+        subscribe: subscribeAutocomplete,
+        setCompletionPrefix,
+        completionCycle,
+    } = useAutocompleter(wordList);
 
     return {
         rows: [initialMessage] as Message[],
@@ -74,32 +52,31 @@ Alpine.data('balmi', () => {
             this.chatInput = '';
         },
 
-        compIdx: 0,
-        localCompletions: [],
+        completions: [],
+        completionIndex: 0,
         onComplete(idx?: number) {
             if (typeof idx === 'number') {
-                this.chatInput = this.localCompletions[idx];
+                this.chatInput = this.completions[idx];
                 this.clearComplete();
                 this.chatInputEl().focus();
                 return;
             }
 
             // make sure the subject is synced to state
-            completionPrefix$.next(this.chatInput);
+            setCompletionPrefix(this.chatInput);
             // emit an increment event
-            incrementCompIdx$.next();
+            completionCycle();
         },
         clearComplete() {
-            completionPrefix$.next('');
+            setCompletionPrefix('');
         },
 
         init() {
-            subscriptions.push(compIdx$.subscribe(compIdx => this.compIdx = compIdx));
-
-            completionPrefix$.next('');
-            subscriptions.push(
-                localCompletions$.subscribe(localCompletions => this.localCompletions = localCompletions)
-            );
+            subscriptions.push(subscribeAutocomplete(
+                ([completions, completionIndex]) => {
+                    this.completions = completions;
+                    this.completionIndex = completionIndex;
+            }));
         },
         destroy() {
             while (subscriptions.length) {
