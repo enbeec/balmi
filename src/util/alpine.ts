023395def19850ac8aad39a4aa4af98e1adbad47
Val Currie <contact@valcurrie.com>
2023-06-30 19:06:34 -0500

    clarified doc on  helper

--- src/util/alpine.ts
+++ src/util/alpine.ts
@@ -3,10 +3,15 @@ import { AlpineComponent } from "alpinejs";
 /** Should match Alpine's internal type of the same name */
 export type XDataContext<T = Record<string,any>> = T & {
     init?(): void;
+    destroy?(): void;
 };
 
-/** TODO: file a bug with https://github.com/DefinitelyTyped/DefinitelyTyped
- * because $store's type is *wrong* and this is my workaround */
+/** You have to use dot syntax to index this.$store so this can be used to set that up.
+ * 
+ * @example 
+ * const thingy = $store<Thingy,'someKey'>(this).someKey;
+ * 
+ * TODO: file a bug with DefinitelyTyped because $store's type is *wrong* and this is my workaround */
 export function $store<T, K extends string>(component: AlpineComponent) {
     return component.$store as { [k in K]: T }
 }
